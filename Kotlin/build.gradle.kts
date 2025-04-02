95cfaf83fce5f6123a60b4fa97a5ef5d4aad2900
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2025-04-02 12:24:46 +0200

    Create a client application in Kotlin using the Ktor framework that allows sending messages to the Discord platform

--- Kotlin/build.gradle.kts
+++ Kotlin/build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
     kotlin("jvm") version "2.1.10"
+    kotlin("plugin.serialization") version "2.1.20"
     application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+val ktor_version: String by project
+val kdor_version: String by project
 
 application {
     mainClass = "MainKt" 
@@ -12,11 +16,19 @@ application {
 
 repositories {
     mavenCentral()
+    // maven("https://repo.kord.dev/snapshots")
 }
 
-// dependencies {
-    //
-// }
+dependencies {
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+    implementation("com.typesafe:config:1.4.3")
+
+    // implementation("dev.kord:kord-core:$kdor_version")
+}
 
 kotlin {
     jvmToolchain(21)
--- Kotlin/src/main/kotlin/Main.kt
+++ Kotlin/src/main/kotlin/Main.kt
@@ -1,5 +1,41 @@
-package org.example
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import io.ktor.http.*
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.Serializable
 
-fun main() {
-    println("Hello World!")
+const val baseUrl = "https://discord.com/api/v10"
+
+@Serializable
+data class DiscordMessage(val content: String)
+
+suspend fun main(args: Array<String>) {
+    val config: Config = ConfigFactory.load()
+
+    val discordAuthToken: String = config.getString("services.discord.token")
+    val discordChannelID: Long = config.getLong("services.discord.channel_id")
+
+    val message = args.getOrNull(0) ?: "Hello everyone!"
+
+    val client = HttpClient(CIO) {
+         install(ContentNegotiation) {
+            json()
+        }
+    }
+
+    val response: HttpResponse = client.post(baseUrl + "/channels/" + discordChannelID +"/messages") {
+        contentType(ContentType.Application.Json)
+        headers {
+            append(HttpHeaders.Authorization, "Bot " + discordAuthToken)
+        }
+        setBody(DiscordMessage(content = message))
+    }
+
+    println(response.status)
+    client.close()
 }
