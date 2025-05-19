d0e9dedc10ab986826dd001b38b4646a8de379bd
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2025-05-19 21:18:00 +0200

    Install Tailwindcss & Some fixes

--- Oauth2/server/src/app.js
+++ Oauth2/server/src/app.js
@@ -1,7 +1,16 @@
 import express from 'express'
+import cors from 'cors'
+
+import routes from '#api'
 
 const app = express()
 const port = process.env.PORT || 8000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.use('/api', routes)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port} 🔥🔥🔥`)
