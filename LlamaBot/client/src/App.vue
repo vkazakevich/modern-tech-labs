cb2791296c39d3ea76ae9ece89d0451f8e22049f
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2025-06-02 23:12:38 +0200

    CSS styles

--- LlamaBot/client/src/App.vue
+++ LlamaBot/client/src/App.vue
@@ -1,27 +1,37 @@
 <template>
-  <div class="mx-auto max-w-100 py-3">
-    <ul v-if="messages.length">
+  <div class="mx-auto max-w-[600px] py-3 border border-gray-200 p-6">
+    <ul
+      class="w-full max-h-[900px] h-[900px] overflow-y-auto flex flex-col"
+      ref="messages"
+    >
       <li
         v-for="message in messages"
-        class="border border-green-300 p-3 mb-5 w-70 rounded"
+        class="border p-3 mb-5 w-70 rounded w-full max-w-[360px]"
+        :class="{
+          'ml-auto border-blue-300': message.isAgent,
+          'border-green-300': !message.isAgent
+        }"
       >
         <strong>{{ message.isAgent ? 'Agent' : 'You' }}:</strong>
         {{ message.content }}
       </li>
-    </ul>
 
-    <p v-if="submitting" class="border border-green-300 p-3 mb-5 w-70 rounded">
-      <strong>Agent:</strong> writing...
-    </p>
+      <p
+        v-if="submitting"
+        class="border border-green-300 p-3 mb-5 w-70 rounded w-full max-w-[360px] ml-auto"
+      >
+        <strong>Agent:</strong> writing...
+      </p>
+    </ul>
 
     <form @submit.prevent="submit">
       <textarea
         v-model="form.content"
-        class="p-3 border border-blue-500 rounded"
+        class="p-3 border border-blue-500 rounded w-full"
       />
       <button
         type="submit"
-        class="block p-3 rounded border border-blue-400 w-30"
+        class="block p-3 rounded border border-blue-400 w-30 w-full"
         :disabled="submitting"
       >
         Send
@@ -48,22 +58,29 @@ export default {
     async submit() {
       this.submitting = true
 
-      this.addMessage(this.form.content)
+      const content = this.form.content
+      this.clearForm()
+
+      this.addMessage(content)
 
       const { data } = await axios.post(`${BASE_URL}/send_question`, {
-        content: this.form.content
+        content
       })
 
       this.addMessage(data.content, true)
 
       this.submitting = false
-      this.clearForm()
     },
 
     addMessage(content, isAgent = false) {
       this.messages.push({
         content,
         isAgent
+      })
+
+      this.$nextTick(() => {
+        const el = this.$refs.messages
+        el.scrollTop = el.scrollHeight
       })
     },
 
