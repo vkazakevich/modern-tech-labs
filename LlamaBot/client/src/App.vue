ca74c3f3af43cd29f5f566220b5294289327e1d3
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2025-06-02 22:51:52 +0200

    Create a frontend interface for the user that communicates with the service; responses should be sent to the frontend interface

--- LlamaBot/client/src/App.vue
+++ LlamaBot/client/src/App.vue
@@ -1,30 +1,75 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="mx-auto max-w-100 py-3">
+    <ul v-if="messages.length">
+      <li
+        v-for="message in messages"
+        class="border border-green-300 p-3 mb-5 w-70 rounded"
+      >
+        <strong>{{ message.isAgent ? 'Agent' : 'You' }}:</strong>
+        {{ message.content }}
+      </li>
+    </ul>
+
+    <p v-if="submitting" class="border border-green-300 p-3 mb-5 w-70 rounded">
+      <strong>Agent:</strong> writing...
+    </p>
+
+    <form @submit.prevent="submit">
+      <textarea
+        v-model="form.content"
+        class="p-3 border border-blue-500 rounded"
+      />
+      <button
+        type="submit"
+        class="block p-3 rounded border border-blue-400 w-30"
+        :disabled="submitting"
+      >
+        Send
+      </button>
+    </form>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:8000'
+
+export default {
+  data: () => ({
+    form: {
+      content: null
+    },
+    messages: [],
+    submitting: false
+  }),
+
+  methods: {
+    async submit() {
+      this.submitting = true
+
+      this.addMessage(this.form.content)
+
+      const { data } = await axios.post(`${BASE_URL}/send_question`, {
+        content: this.form.content
+      })
+
+      this.addMessage(data.content, true)
+
+      this.submitting = false
+      this.clearForm()
+    },
+
+    addMessage(content, isAgent = false) {
+      this.messages.push({
+        content,
+        isAgent
+      })
+    },
+
+    clearForm() {
+      this.form.content = null
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
--- LlamaBot/server/main.py
+++ LlamaBot/server/main.py
@@ -1,22 +1,33 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from ollama import chat, Client, ChatResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+class Question(BaseModel):
+    content: str
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.post("/chat")
-def request_chat(content: str):
+@app.post("/send_question")
+def send_question(question: Question):
     client = Client(host='http://host.docker.internal:11434')
 
     response: ChatResponse = client.chat(model='llama3.2', messages=[
         {
             'role': 'user',
-            'content': content,
+            'content': question.content,
         },
     ])
 
-    return response.message.content
+    return {
+        "content": response.message.content
+    }
