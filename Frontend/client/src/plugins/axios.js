892f694cd19364ce5781c4058f76f6dcfe8615a0
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2025-06-05 18:34:20 +0200

    Update routes

--- Frontend/client/src/plugins/axios.js
+++ Frontend/client/src/plugins/axios.js
@@ -3,3 +3,7 @@ import axios from 'axios'
 if (process.env.NODE_ENV === 'development') {
   axios.defaults.baseURL = 'http://localhost:8001'
 }
+
+if (process.env.NODE_ENV === 'production') {
+  axios.defaults.baseURL = 'https://ebiznes-back-end-e2gfc5ducudeg8fv.polandcentral-01.azurewebsites.net'
+}
